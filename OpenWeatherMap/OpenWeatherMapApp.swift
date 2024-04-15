2f09bcb9cc6dc158c4d73f487b53a72aa5b0bf15
David Alarcon <momachilles@gmail.com>
2024-04-15 12:45:24 +0200

    Initial Commit

---
+++ OpenWeatherMap/OpenWeatherMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherMapApp.swift
+//  OpenWeatherMap
+//
+//  Created by David Alarcon on 15/4/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
