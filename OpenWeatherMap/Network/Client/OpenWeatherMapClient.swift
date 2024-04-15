039b91407726d80b98ef30b246875351b267801a
David <momachilles@gmail.com>
2024-04-15 18:02:10 +0200

    Add an empty network client.

---
+++ OpenWeatherMap/Network/Client/OpenWeatherMapClient.swift
@@ -0,0 +1,16 @@
+//
+//  OpenWeatherMapClient.swift
+//  OpenWeatherMap
+//
+//  Created by David Alarcon on 15/4/24.
+//
+
+import Foundation
+
+protocol FiveDaysForecastAPI {
+  func fiveDaysForeCast()
+}
+
+class OpenWeatherMapClient {
+  
+}
