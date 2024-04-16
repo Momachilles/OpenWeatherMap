9dd6cab1aa1cb1a145a33170ea1e7b2adf88a3d5
David <momachilles@gmail.com>
2024-04-16 09:10:15 +0200

    Network service using endpoints url requestables.

--- OpenWeatherMap/Network/Service/NetworkService.swift
+++ OpenWeatherMap/Network/Service/NetworkService.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import RxSwift
+
+enum NetworkError: Error {
+  case invalidURL
+  case invalidResponse
+  case requestFailed(Error)
+  // Add more cases as needed
+}
+
+protocol NetworkServiceProtocol {
+  func request<T: Decodable>(_ endpoint: URLRequestable, responseType: T.Type, completion: @escaping (Result<T, NetworkError>) -> Void)
+}
+
+class NetworkService: NetworkServiceProtocol {
+  func request<T: Decodable>(_ endpoint: URLRequestable, responseType: T.Type, completion: @escaping (Result<T, NetworkError>) -> Void) {
+    guard let urlRequest = endpoint.urlRequest else {
+      completion(.failure(.invalidURL))
+      return
+    }
+    
+    URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+      guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+        completion(.failure(.invalidResponse))
+        return
+      }
+      
+      if let error = error {
+        completion(.failure(.requestFailed(error)))
+        return
+      }
+      
+      guard let data = data else {
+        completion(.failure(.invalidResponse))
+        return
+      }
+      
+      do {
+        let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+        completion(.success(decodedResponse))
+      } catch {
+        completion(.failure(.invalidResponse))
+      }
+    }.resume()
+  }
+}
