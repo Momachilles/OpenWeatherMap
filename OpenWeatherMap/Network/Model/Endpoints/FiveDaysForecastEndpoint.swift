95700265d26eeeedbd92fcfc7c35f7eaeb668f15
David <momachilles@gmail.com>
2024-04-16 11:17:25 +0200

    To fix the the conversion from urlcomponents to url.

--- OpenWeatherMap/Network/Model/Endpoints/FiveDaysForecastEndpoint.swift
+++ OpenWeatherMap/Network/Model/Endpoints/FiveDaysForecastEndpoint.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct FiveDaysForecastEndpoint: OpenWeatherMapEndpoint {
   var city: String
-  var path: String { "forecast" }
+  var path: String { "/forecast" }
   var queryParameters: [String : String] { ["q": city, "appid": "73d4241c301a36ae37bcf003d9c78279"] }
   var method: Method { .get }
   
@@ -17,7 +17,7 @@ struct FiveDaysForecastEndpoint: OpenWeatherMapEndpoint {
     var components = URLComponents()
     components.scheme = scheme
     components.host = host
-    components.path = baseURLString + "/" + path
+    components.path = baseURLString + path
     components.queryItems = queryParameters.map { URLQueryItem(name: $0.key, value: $0.value) }
     
     guard let url = components.url else { return .none }
