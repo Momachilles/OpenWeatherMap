c0c2ed1d78d0b67e21059fd94f233a2639a39a43
David <momachilles@gmail.com>
2024-04-18 10:05:53 +0200

    Create network service tests and related stuff in order to work. Just successfull response.

---
+++ OpenWeatherMapTests/Network/Dummies/Model/DummyTestModel.swift
@@ -0,0 +1,16 @@
+//
+//  MockTestModel.swift
+//  OpenWeatherMapTests
+//
+//  Created by David Alarcon on 18/4/24.
+//
+
+import Foundation
+
+struct DummyTestModel: Codable {
+  var message: String
+}
+
+extension DummyTestModel {
+  var json: String { "{\"message\": \"\(message)\"}"}
+}
---
+++ OpenWeatherMapTests/Network/Service/NetworkServiceTests.swift
@@ -0,0 +1,67 @@
+//
+//  NetworkServiceTests.swift
+//  OpenWeatherMapTests
+//
+//  Created by David Alarcon on 18/4/24.
+//
+
+import XCTest
+import RxSwift
+@testable import OpenWeatherMap
+
+final class NetworkServiceTests: XCTestCase {
+
+    func testNetworkServiceWhenSuccessfulResponse() throws {
+      /// Given
+      let sut = NetworkServiceTests.mockNetworkService()
+      
+      guard let response = try? NetworkServiceTests.successfullResponse()
+      else { return XCTFail("Something went wrong.") }
+      MockURLProtocol.mockResponses = response
+      
+      /// When
+      let expectation = XCTestExpectation(description: "Request completes successfully")
+      let single = sut.request(from: DummyEndpoint()) as Single<DummyTestModel>
+      
+      /// Then
+      _ = single.subscribe { event in
+        switch event {
+        case .success(let response): XCTAssertEqual(response.message, "Success")
+        case .failure(let error): XCTFail("Request failed with error: \(error)")
+        }
+        expectation.fulfill()
+      }
+      
+      wait(for: [expectation], timeout: 1.0)
+    }
+}
+
+extension NetworkServiceTests {
+  static func mockNetworkService() -> NetworkServiceProtocol {
+    URLProtocol.registerClass(MockURLProtocol.self)
+    
+    let configuration = URLSessionConfiguration.default
+    configuration.protocolClasses = [MockURLProtocol.self]
+    
+    return NetworkService(configuration: configuration)
+  }
+  
+  static func successfullResponse() throws -> [URL: (Data?, URLResponse?, Error?)]? {
+    guard let url = DummyEndpoint().urlRequest?.url else { throw NetworkError.invalidURL }
+          
+    let expectedData = DummyTestModel(message: "Success").json.data(using: .utf8)
+    let expectedResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+    
+    return [url: (expectedData, expectedResponse, .none)]
+  }
+}
+
+extension DummyFiveDaysForecastLoader { // TODO: Delete
+  static func loadJSONData() throws -> Data? {
+    guard let url = Bundle.main.url(forResource: "FiveDaysForecast", withExtension: "json") else {
+      throw NSError(domain: "DummyFiveDaysForecastLoader", code: 404, userInfo: [NSLocalizedDescriptionKey: "File not found"])
+    }
+    
+    return try Data(contentsOf: url)
+  }
+}
